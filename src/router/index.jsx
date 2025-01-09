10d33cef42382e93ce6099b060b70f1d79ac1cb6
aditya <asadityasingh2572000@gmail.com>
2025-01-09 22:18:04 +0530

    change routing for other components

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,66 +1,52 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Footer from '../components/Footer';
-import { Navigate } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
-import Choose from '../components/Choose';
-import Nearby from '../components/Nearby';
-import Features from '../components/Features';
-import Contact from '../components/Contact';
-import Projects from '../components/Projects';
-import AboutUs from '../components/AboutUs';
-import Project from '../components/Project';
-import Review from '../components/Review';
-import Services from '../components/Services';
-
+import { createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+import { Navigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Contact from '../components/Contact'
+import AboutUs from '../components/AboutUs'
+import Services from '../components/Services'
+import WhyChooseUs from '../components/WhyChooseUs'
+import CustomerReviews from '../components/CustomerReviews'
+import BestProducts from '../components/BestProducts'
+import Why from '../components/Why'
+import Review from '../components/Review'
 
 export const router = createBrowserRouter([
-
-  
   {
     path: '/',
     element: <App />,
     children: [
-    
-    
       {
         path: '',
         element: (
           <>
             <Navbar />
             <Hero />
-            <Review/>
-            <Choose/>
-            <Nearby/>
-            <Features/>
-          <Projects/>
-          <Footer/>
+            <Review />
+            <WhyChooseUs />
+            <CustomerReviews />
+            <BestProducts />
+            <Why />
+            <Footer />
           </>
         ),
       },
       {
-        path: 'choose',
-        element:<Choose/>    
-      },
-      {
         path: 'Contact',
-        element:<Contact/>   
+        element: <Contact />,
       },
-      
+
       {
         path: '/aboutUs',
-        element:<AboutUs/>   
-      },
-      {
-        path: '/project',
-        element:<Project/>   
+        element: <AboutUs />,
       },
       {
         path: '/services',
-        element:<Services/>   
+        element: <Services />,
       },
     ],
   },
-]);
+])
